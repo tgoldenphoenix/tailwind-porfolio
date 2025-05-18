e11519ccd6434eb25894d3590cbfd8ac445ec1c3
tgoldenphoenix <anhaophamx@gmail.com>
2025-05-18 09:45:36 +0700

    added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return ( 
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element/>
-          <Route />
+          <Route index element={<Home />}/>
+          {/* the NotFound path is the last Route */}
+          <Route path="*" element={<NotFound />}/>
         </Routes>
       </BrowserRouter>
     </>
