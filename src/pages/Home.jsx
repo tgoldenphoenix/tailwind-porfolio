82e1a6e10a7288b5068256f6b5ea087b505212ca
tgoldenphoenix <anhaophamx@gmail.com>
2025-05-21 13:22:13 +0700

    add projects section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { AboutSection } from "../components/AboutSection"
 import { HeroSection } from "../components/HeroSection"
 import { Navbar } from "../components/Navbar"
+import { ProjectsSection } from "../components/ProjectsSection"
 import { SkillsSection } from "../components/SkillsSection"
 import { ThemeToggle } from "../components/ThemeToggle"
 import { StarBackground } from "@/components/StarBackground"
@@ -19,6 +20,7 @@ export const Home = () => {
       <HeroSection />
       <AboutSection />
       <SkillsSection />
+      <ProjectsSection />
     </main>
 
     {/* Footer */}
