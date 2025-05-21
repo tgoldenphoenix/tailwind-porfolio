9b6ca80babe9744747c1d8350de0c79eb0c2b622
tgoldenphoenix <anhaophamx@gmail.com>
2025-05-21 12:43:35 +0700

    add skills section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { AboutSection } from "../components/AboutSection"
 import { HeroSection } from "../components/HeroSection"
 import { Navbar } from "../components/Navbar"
+import { SkillsSection } from "../components/SkillsSection"
 import { ThemeToggle } from "../components/ThemeToggle"
 import { StarBackground } from "@/components/StarBackground"
 
@@ -17,6 +18,7 @@ export const Home = () => {
     <main>
       <HeroSection />
       <AboutSection />
+      <SkillsSection />
     </main>
 
     {/* Footer */}
