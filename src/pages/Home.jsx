c58243726fee47afebbc80d39be19d6c504f237b
tgoldenphoenix <anhaophamx@gmail.com>
2025-05-23 14:06:50 +0700

    change light & dark theming option

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,8 @@ import { StarBackground } from "@/components/StarBackground"
 
 export const Home = () => {
   return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-    {/* Theme Toggle */}
-    <ThemeToggle />
+    {/* <ThemeToggle /> */}
+
     {/* Background Effects */}
     <StarBackground />
     {/* Navbar */}
