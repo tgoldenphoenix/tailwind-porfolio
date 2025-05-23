e2f17d85a644e2d575ff5b89cb0f93376fb23231
tgoldenphoenix <anhaophamx@gmail.com>
2025-05-23 19:22:42 +0700

    update skill section

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,103 +1,39 @@
 import { useState } from "react"
 import { cn } from "@/lib/utils"
 
-const skills = [
-  // Frontend
-  { name: "HTML/CSS", level: 95, category: "frontend" },
-  { name: "JavaScript", level: 90, category: "frontend" },
-  { name: "React", level: 90, category: "frontend" },
-  { name: "TypeScript", level: 85, category: "frontend" },
-  { name: "Tailwind CSS", level: 90, category: "frontend" },
-  { name: "Next.js", level: 80, category: "frontend" },
-
-  // Backend
-  { name: "Node.js", level: 80, category: "backend" },
-  { name: "Express", level: 75, category: "backend" },
-  { name: "MongoDB", level: 70, category: "backend" },
-  { name: "PostgreSQL", level: 65, category: "backend" },
-  { name: "GraphQL", level: 60, category: "backend" },
-
-  // Tools
-  { name: "Git/GitHub", level: 90, category: "tools" },
-  { name: "Docker", level: 70, category: "tools" },
-  { name: "Figma", level: 85, category: "tools" },
-  { name: "VS Code", level: 95, category: "tools" },
-]
-
 const skills2 = [
   // Frontend
-  { name: "devicon-javascript-plain colored", category: "frontend" },
-  { name: "devicon-typescript-plain colored", category: "frontend" },
-  { name: "devicon-react-original colored", category: "frontend" },
-
-  //
+  { name: "devicon-html5-plain-wordmark colored", title: "HTML5" },
+  { name: "devicon-css3-plain-wordmark colored", title: "CSS3" },
+  { name: "devicon-javascript-plain colored", title: "JavaScript" },
+  { name: "devicon-typescript-plain colored", title: "TypeScript" },
+  { name: "devicon-react-original colored", title: "ReactJS" },
+  { name: "devicon-nodejs-plain-wordmark colored", title: "NodeJS" },
+  { name: "devicon-postgresql-plain colored", title: "PostgreSQL" },
+  { name: "devicon-materialui-plain colore", title: "Material UI" },
+  { name: "devicon-tailwindcss-original colored", title: "TailwindCSS" },
+  { name: "devicon-github-original", title: "Git" },
 ]
 
-const categories = ["all", "frontend", "backend", "tools"]
-
 export const SkillsSection = () => {
-  const [activeCategory, setActiveCategory] = useState("all")
-
-  const filteredSkills = skills.filter(
-    (skill) => activeCategory === "all" || skill.category === activeCategory
-  )
 
   return (
     <section id="skills" className="py-24 px-4 relative bg-secondary/30">
       <div className="container mx-auto max-w-5xl">
-        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+        <h2 className="text-3xl md:text-4xl font-bold mb-7 text-center">
           My <span className="text-primary"> Skills</span>
         </h2>
 
-        <div className="flex flex-wrap justify-center gap-4 mb-12">
-          {categories.map((category, key) => (
-            <button
-              key={key}
-              onClick={() => setActiveCategory(category)}
-              className={cn(
-                "px-5 py-2 rounded-full transition-colors duration-300 capitalize",
-                activeCategory === category
-                  ? "bg-primary text-primary-foreground"
-                  : "bg-secondary/70 text-forefround hover:bd-secondary"
-              )}
-            >
-              {category}
-            </button>
-          ))}
-        </div>
-        
+        <div className="font-medium text-2xl mb-5">Programming Languages & Tools:</div>
+
         <ul className="text-[3rem] list-none"> 
           {skills2.map((skill) => (  
-            <li className="inline-block mr-[1rem]">
-              <i class={skill.name}></i>
+            <li className="relative inline-block mr-[1rem]">
+              <i className={"skill-badge hover:text-red "+skill.name} title={skill.title}></i>
             </li>
           ))}
         </ul>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {filteredSkills.map((skill, key) => (
-            <div
-              key={key}
-              className="bg-card p-6 rounded-lg shadow-xs card-hover"
-            >
-              <div className="text-left mb-4">
-                <h3 className="font-semibold text-lg"> {skill.name}</h3>
-              </div>
-              <div className="w-full bg-secondary/50 h-2 rounded-full overflow-hidden">
-                <div
-                  className="bg-primary h-2 rounded-full origin-left animate-[grow_1.5s_ease-out]"
-                  style={{ width: skill.level + "%" }}
-                />
-              </div>
-
-              <div className="text-right mt-1">
-                <span className="text-sm text-muted-foreground">
-                  {skill.level}%
-                </span>
-              </div>
-            </div>
-          ))}
-        </div>
       </div>
     </section>
   )
